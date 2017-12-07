9f43f1c4418b6b77882bbd1e4e4294bbbee84bcf
kazuya <aqua.timez.gto@gmail.com>
2017-12-08 00:09:01 +0900

    tabインクルード処理

--- app/src/main/java/com/example/aquat/tabscreen/TabMainActivity.java
+++ app/src/main/java/com/example/aquat/tabscreen/TabMainActivity.java
@@ -11,6 +11,25 @@ import android.widget.Button;
 
 public class TabMainActivity extends FragmentActivity {
 
+    //region 定義
+    public enum Page {
+        Comment(0),
+        Ditail(1),;
+
+        private final int id;
+
+        private Page(final int id) {
+            this.id = id;
+        }
+
+        public int getInt() {
+            return  this.id;
+        }
+
+
+    }
+    //endregion
+
     ViewPager viewPager;
 
     @Override
@@ -23,25 +42,25 @@ public class TabMainActivity extends FragmentActivity {
                         getSupportFragmentManager()));
 
         //タブボタン設定
-       /* ViewGroup vg = (ViewGroup)findViewById(R.id.tab);
+        ViewGroup vg = (ViewGroup)findViewById(R.id.tab);
         //Layout取得
         getLayoutInflater().inflate(R.layout.tab_layout,vg);
         Button commentBtn = (Button)(vg.findViewById(R.id.tab_comment));
-        Button ditailBtn = (Button)(vg.findViewById(R.id.tab_ditail));*/
-        Button commentBtn = (Button)(findViewById(R.id.tab_comment));
-        Button ditailBtn = (Button)(findViewById(R.id.tab_ditail));
+        Button ditailBtn = (Button)(vg.findViewById(R.id.tab_ditail));
+       /* Button commentBtn = (Button)(findViewById(R.id.tab_comment));
+        Button ditailBtn = (Button)(findViewById(R.id.tab_ditail));*/
 
         commentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                viewPager.setCurrentItem(0);
+                viewPager.setCurrentItem(Page.Comment.getInt());
             }
         });
 
         ditailBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                viewPager.setCurrentItem(1);
+                viewPager.setCurrentItem(Page.Ditail.getInt());
             }
         });
         //Button comentBtn =
